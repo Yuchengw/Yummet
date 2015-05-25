d704f3977c0c1f51a6c5be593d7646e0fa1ec36d
Mingmin Chen <mikemmchen@gmail.com>
2015-05-25 01:08:16 -0700

    resolved conflicts

--- Webapp/app/v1/core/appserver/src/main/webapp/resources/angular/app/app_config.js
+++ Webapp/app/v1/core/appserver/src/main/webapp/resources/angular/app/app_config.js
@@ -1,30 +1,5 @@
 /**
  * @author yucheng
-<<<<<<< HEAD
- * @version 1
- */
-angular.module('yummet.home',['ngRoute','signupApp'])
-.config(function($routeProvider){
-      $routeProvider
-          .when('/login',{
-                templateUrl: '/rs/angular/app/components/identification/login/login.amher',
-                controller: 'loginAppContoller'
-          })
-          .when('/signup',{
-                templateUrl: '/rs/angular/app/components/identification/signup/signup.amher',
-                controller: 'signupAppController'
-          })
-          .when('/amherpost',{
-       	  	templateUrl: '/rs/angular/app/components/post/post-content.amher',
-                controller: 'postAppController'
-          });
-})
-.controller('cfgController',function($scope){
-    /*
-	 * Here you can handle controller for specific route as well.
-	 */
-});
-=======
  * @since 1
  */
 var app = angular.module('yummet',['ngRoute','ngResource','signupApp','loginApp','filterApp', 'postApp'])
@@ -72,5 +47,4 @@ app.config(function($routeProvider, $httpProvider){
 	};
 	
 }]);
->>>>>>> origin/yucheng-topic
 
